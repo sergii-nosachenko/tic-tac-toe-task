0fe8c54730afdd171ab84eb46a3cfaa8db91f7d0
sergii-nosachenko <s.nosachenko@gmail.com>
2023-07-17 13:31:11 +0300

    Removed solution import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from '@mui/material';
-import Game from './components/Game.solution';
+import Game from './components/Game';
 
 export default function App() {
   return (
